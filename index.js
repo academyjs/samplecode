91c89cb7227b09b2c9d75239daed404098a91755
academyjs <academyjs2016@gmail.com>
2016-06-12 10:44:55 +0100

    Update index.js
    Fixed capitilisation

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ if (!process.argv[2]) {
 // parse application/json
 app.use(bodyParser.json());
 
-var instructionSetResource = require('./libs/rest/InstructionSetResource');
+var instructionSetResource = require('./libs/rest/instructionSetResource');
 var instructionSetExecutionResource = require('./libs/rest/instructionSetExecutionResource');
 
 app.listen(3000, function () {
